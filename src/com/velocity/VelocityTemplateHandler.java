34ec87743b52504ba2ed084baf5127ced020d49d
Muhammad Shahbaz Akbar <makbar@i2c.com>
2021-08-20 20:30:14 +0500

    Changes Committed

--- src/com/velocity/VelocityTemplateHandler.java
+++ src/com/velocity/VelocityTemplateHandler.java
@@ -3,6 +3,9 @@ import com.velocity.pojo.FlightLegC;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.tools.generic.ConversionTool;
+import org.apache.velocity.tools.generic.DateTool;
+import org.apache.velocity.tools.generic.SortTool;
 
 import java.io.StringWriter;
 import java.util.*;
@@ -10,7 +13,7 @@ import java.util.*;
 public class VelocityTemplateHandler {
     public static void main(String[] args) {
         String directory = "D:\\applications-inteliJ\\TestApp\\src\\com\\velocity\\templates";
-        String template = "flight.vm";
+        String template = "To__c.txt";
         Properties p = new Properties();
         VelocityEngine veEngine = new VelocityEngine();
 
@@ -35,6 +38,9 @@ public class VelocityTemplateHandler {
     private static String processTemplate(VelocityEngine veEngine, Template templateObj, List<FlightLegC> flightLegCList ){
 
         VelocityContext vc = new VelocityContext();
+        vc.put("date", new DateTool());
+        vc.put("sorter", new SortTool());
+        vc.put("convert", new ConversionTool());
 
 
         vc.put("Flight_Leg__cList", flightLegCList);
@@ -50,22 +56,58 @@ public class VelocityTemplateHandler {
 
         List<FlightLegC> flightLegCList = new ArrayList<FlightLegC>();
         Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, 27);
+        cal.set(Calendar.MONTH, 7);
         cal.set(Calendar.YEAR, 2021);
+        cal.set(Calendar.HOUR, 10);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.AM_PM, 0);
         Date d1 = cal.getTime();
-        cal.set(Calendar.YEAR, 2022);
+
+        cal.set(Calendar.DAY_OF_MONTH, 20);
+        cal.set(Calendar.MONTH, 7);
+        cal.set(Calendar.YEAR, 2021);
+        cal.set(Calendar.HOUR, 6);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.AM_PM, 0);
         Date d2 = cal.getTime();
-        cal.set(Calendar.YEAR, 2023);
+
+        cal.set(Calendar.DAY_OF_MONTH, 27);
+        cal.set(Calendar.MONTH, 7);
+        cal.set(Calendar.YEAR, 2021);
+        cal.set(Calendar.HOUR, 10);
+        cal.set(Calendar.MINUTE, 45);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.AM_PM, 0);
         Date d3 = cal.getTime();
 
+        cal.set(Calendar.DAY_OF_MONTH, 20);
+        cal.set(Calendar.MONTH, 7);
+        cal.set(Calendar.YEAR, 2021);
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 7);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.AM_PM, 0);
+        Date d4 = cal.getTime();
+   /* System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(d3);
+    System.out.println(d4);
+*/
+        FlightLegC flightLegC2 = new FlightLegC("Booked", d2, d4, "KTEB", "KSAN");
+        FlightLegC flightLegC1 = new FlightLegC("Booked", d1, d3, "KSAN", "KTEB");
 
-
-        FlightLegC flightLegC1 = new FlightLegC("Booked", d2, "test1@test.com");
-        FlightLegC flightLegC2 = new FlightLegC("Booked", d1, "test2@test.com");
-        FlightLegC flightLegC3 = new FlightLegC("Booked", d3, "test3@test.com");
+        // FlightLegC flightLegC3 = new FlightLegC("Booked", d3, "KILM",  "MYEH");
+        //FlightLegC flightLegC4 = new FlightLegC("Booked", d4, "Islamabad",  "KGPT");
+        flightLegCList.add(flightLegC2);
 
         flightLegCList.add(flightLegC1);
-        flightLegCList.add(flightLegC2);
-        flightLegCList.add(flightLegC3);
+
+
+        //   flightLegCList.add(flightLegC3);
+     //   flightLegCList.add(flightLegC4);
         return flightLegCList;
     }
 
--- src/com/velocity/pojo/FlightLegC.java
+++ src/com/velocity/pojo/FlightLegC.java
@@ -1,43 +1,66 @@
 package com.velocity.pojo;
+import java.util.Calendar;
 import java.util.Date;
 
 public class FlightLegC {
 
-
-    private  String status__c;
-    private Date departure_Time__c;
-    private  String to__c;
+    private  String Status__c;
+    private Date Est_Departure_Time_EST__c;
+    private Date Est_Arrival_Time_EST__c;
+    private Calendar calDeparture;
+    private String From__c;
+    private  String To__c;
     public FlightLegC(){}
-    public FlightLegC(String status, Date departureTime, String toC){
-        status__c = status;
-        departure_Time__c = departureTime;
-        to__c = toC;
+    public FlightLegC(String status, Date departureTime, Date est_Arrival_Time_EST, String fromC, String toC){
+        Status__c = status;
+        Est_Departure_Time_EST__c = departureTime;
+        To__c = toC;
+        From__c = fromC;
+        Est_Arrival_Time_EST__c = est_Arrival_Time_EST;
     }
 
 
     public String getStatus__c() {
-        return status__c;
+        return Status__c;
     }
 
     public void setStatus__c(String status__c) {
-        this.status__c = status__c;
+        this.Status__c = status__c;
     }
 
-    public Date getDeparture_Time__c() {
-        return departure_Time__c;
+    public Date getEst_Departure_Time_EST__c() {
+        return Est_Departure_Time_EST__c;
     }
 
-    public void setDeparture_Time__c(Date departure_Time__c) {
-        this.departure_Time__c = departure_Time__c;
+    public void setEst_Departure_Time_EST__c(Date est_Departure_Time_EST__c) {
+        this.Est_Departure_Time_EST__c = est_Departure_Time_EST__c;
     }
 
+    public String getFrom__c() {    return From__c;    }
+
+    public void setFrom__c(String from__c) {    From__c = from__c;
+    }
     public String getTo__c() {
-        return to__c;
+        return To__c;
     }
 
     public void setTo__c(String to__c) {
-        this.to__c = to__c;
+        this.To__c = to__c;
     }
 
+    public Date getEst_Arrival_Time_EST__c() {
+        return Est_Arrival_Time_EST__c;
+    }
 
+    public void setEst_Arrival_Time_EST__c(Date est_Arrival_Time_EST__c) {
+        Est_Arrival_Time_EST__c = est_Arrival_Time_EST__c;
+    }
+
+    public Calendar getCalDeparture() {
+        return calDeparture;
+    }
+
+    public void setCalDeparture(Calendar calDeparture) {
+        this.calDeparture = calDeparture;
+    }
 }
